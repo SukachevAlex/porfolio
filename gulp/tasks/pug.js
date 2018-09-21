6c41c51008dd7dc0bb9ccda52f1d65c44eb8932b
SukachevAlex <sukachevalexandr@gmail.com>
2018-09-21 14:37:26 +0300

    change project architecture, add blog, portfolio, contact pages

--- gulp/tasks/pug.js
+++ gulp/tasks/pug.js
@@ -1,6 +1,11 @@
 module.exports = function() {
     $.gulp.task('pug', function() {
-        return $.gulp.src('app/project/pages/*.pug')
+        return $.gulp.src([
+                'app/project/pages/index/*.pug',
+                'app/project/pages/portfolio/*.pug',
+                'app/project/pages/blog/*.pug',
+                'app/project/pages/contact/*.pug',
+            ])
             .pipe($.gp.pug({
                 pretty: true
             }))
