ab0bb651115d5e72d5a58b58d8efe41611ffc154
SukachevAlex <sukachevalexandr@gmail.com>
2018-09-26 16:39:11 +0300

    Add pages for all projects

--- gulp/tasks/pug.js
+++ gulp/tasks/pug.js
@@ -6,6 +6,14 @@ module.exports = function() {
                 'app/project/pages/blog/*.pug',
                 'app/project/pages/contact/*.pug',
                 'app/project/pages/skills/*.pug',
+                'app/project/pages/projects/project-agriplus/*.pug',
+                'app/project/pages/projects/project-domYandex/*.pug',
+                'app/project/pages/projects/project-gratia/*.pug',
+                'app/project/pages/projects/project-hemma/*.pug',
+                'app/project/pages/projects/project-portfolio/*.pug',
+                'app/project/pages/projects/project-posh/*.pug',
+                'app/project/pages/projects/project-smitler/*.pug',
+                'app/project/pages/projects/project-wedding/*.pug',
             ])
             .pipe($.gp.pug({
                 pretty: true
